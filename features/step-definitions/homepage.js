b94fda0152f82e10afeacee5ecd59ccca9273f86
Zach <zach@192.168.1.99>
2017-04-15 14:37:24 +0100

    changes

--- features/step-definitions/homepage.js
+++ features/step-definitions/homepage.js
@@ -1,21 +1,15 @@
-import * as homepage from '../../pageObjects/homepage'
-
-var chai =require('chai')
+var homepage = require('../../pageObjects/homepage')
 
 module.exports = function () {
 
 
-   
 
-    var assert = chai.assert;
     this.Given(/^I am on the landing page$/,
-    
-    homepage.gotoPage)
-    
-   // this.Then(/^I can see logo in navigation bar$/, assert.equal(1,1))
+
+        homepage.gotoPage)
 
 
     this.Then(/^I can see logo in navigation bar$/,
-    
-    homepage.hasLogo)
+
+          homepage.hasLogo)
 }
